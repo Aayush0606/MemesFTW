ea8bc368721f6c2bf2181222b7348e96db7e901c
AAYUSH SHARMA <aayushsharma0909@gmail.com>
2021-10-18 12:47:24 +0530

    Splash screen and icon remaining

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -1,30 +1,95 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Linking,
+  Share,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import { Card, ListItem, Button, Icon } from "react-native-elements";
+import * as Sharing from "expo-sharing";
+import * as FileSystem from "expo-file-system";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
 import Styles from "../styles/globalStyle";
+import CameraRoll from "@react-native-community/cameraroll";
 
 const CardScreen = ({ item }) => {
-  const url = item.url;
+  const letsShare = async () => {
+    try {
+      const result = await Share.share({
+        message: `Checkout this amazing meme: https://www.reddit.com${item.permalink}`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log(activityType);
+        } else {
+          console.log("Shared");
+        }
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const letsDownload = async () => {
+    const url = item.url.split("/");
+    const pathName = url[url.length - 1];
+    let permissions = await MediaLibrary.requestPermissionsAsync();
+    if (!permissions.granted) {
+      alert("permissions required");
+    }
+    if (permissions.granted) {
+      const download = FileSystem.downloadAsync(
+        item.url,
+        FileSystem.documentDirectory + pathName
+      )
+        .then(async ({ uri }) => {
+          try {
+            const asset = await MediaLibrary.createAssetAsync(uri);
+            await MediaLibrary.createAlbumAsync("MemeHub", asset, false);
+            alert("Saved in pictures");
+          } catch (error) {
+            console.log(error);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <View style={Styles.cardBox}>
       <Card>
         <Card.Title>{item.title}</Card.Title>
         <Card.Divider />
-        <TouchableOpacity
+        <Card.Image
           onPress={() =>
             Linking.openURL(`https://www.reddit.com` + item.permalink)
           }
-        >
-          <Card.Image
-            style={Styles.cardImageStyle}
-            source={{ uri: `${item.url}` }}
-          ></Card.Image>
-        </TouchableOpacity>
+          style={Styles.cardImageStyle}
+          source={{ uri: `${item.url}` }}
+        ></Card.Image>
         <Text style={{ marginBottom: 10 }}>SubReddit: {item.subReddit}</Text>
         <Text style={{ marginBottom: 10 }}>Posted by: {item.author}</Text>
         <Button
+          onPress={letsShare}
           icon={<Icon name="share" type="FontAwesome" />}
           buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+        />
+        <Button
+          onPress={letsDownload}
+          icon={<FontAwesome name="download" size={24} color="black" />}
+          buttonStyle={{
+            marginTop: 10,
             borderRadius: 0,
             marginLeft: 0,
             marginRight: 0,
--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Image, FlatList } from "react-native";
-import { Card, ListItem, Button, Icon } from "react-native-elements";
+import { Card, ListItem, Button, Icon, SearchBar } from "react-native-elements";
 import Styles from "../styles/globalStyle";
 import CardScreen from "./CardScreen";
 import HeaderScreen from "./HeaderScreen";
@@ -9,46 +9,82 @@ const homeScreen = () => {
   const [totalPost, setTotalPost] = useState(0);
   const [data, setData] = useState([]);
   const [after, setAfter] = useState("");
+  const [search, setSearch] = useState("");
+  const [index, setIndex] = useState(0);
+  const [subReddit, setSubReddit] = useState("dankmeme");
+  const [showNext, setShowNext] = useState(false);
 
   const getMyMemes = async () => {
-    let arr = [];
-    const apiRawData = await fetch(
-      `https://www.reddit.com/r/dankmeme/new.json?after=${after}`
-    );
-    const parsedData = await apiRawData.json();
-    setAfter(parsedData.data.after);
-    const postDetails = parsedData.data.children;
-    setTotalPost(parsedData.data.dist);
-    postDetails.forEach((item) => {
-      const detailsToShow = {
-        title: item.data.title,
-        url: item.data.url,
-        subReddit: item.data.subreddit_name_prefixed,
-        permalink: item.data.permalink,
-        author: item.data.author,
-      };
-      arr.push(detailsToShow);
-    });
-    setData(data.concat(arr));
+    try {
+      console.log("entered");
+      let arr = [];
+      const apiRawData = await fetch(
+        `https://www.reddit.com/r/${subReddit}.json?after=${after}`
+      );
+      const parsedData = await apiRawData.json();
+      if (parsedData.data.dist === 0) {
+        console.log("Bad request");
+      } else {
+        setAfter(parsedData.data.after);
+        const postDetails = parsedData.data.children;
+        setTotalPost(parsedData.data.dist);
+        postDetails.forEach((item) => {
+          if (item.data.url) {
+            const detailsToShow = {
+              title: item.data.title,
+              url: item.data.url,
+              subReddit: item.data.subreddit_name_prefixed,
+              permalink: item.data.permalink,
+              author: item.data.author,
+            };
+            arr.push(detailsToShow);
+          }
+        });
+        setData(data.concat(arr));
+      }
+    } catch (error) {
+      setSubReddit("dankmeme");
+      setSearch("");
+      setAfter("");
+      setData([]);
+      console.log("error");
+    }
   };
 
   useEffect(() => {
     getMyMemes();
-  }, []);
+  }, [subReddit]);
 
   return (
     <>
       <HeaderScreen />
+      <SearchBar
+        onSubmitEditing={() => {
+          setData([]);
+          setAfter("");
+          setSubReddit(search);
+          getMyMemes();
+        }}
+        placeholder="Search a subreddit..."
+        onChangeText={(e) => setSearch(e)}
+        value={search}
+        containerStyle={{ backgroundColor: "black" }}
+        inputContainerStyle={{ borderRadius: 50 }}
+      />
+
       {data && (
         <>
           <FlatList
             data={data}
             renderItem={CardScreen}
             keyExtractor={(item) => item.permalink}
+            onEndReached={() => {
+              getMyMemes();
+            }}
           />
-          <Button title="Next" raised onPress={getMyMemes} />
         </>
       )}
+      {showNext && <Button title="Next" raised onPress={getMyMemes} />}
     </>
   );
 };
